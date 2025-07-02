afaf093471c678932c4036d6b2274241c43ccfe3
gportodev <gportodev@gmail.com>
2025-07-02 10:57:19 -0300

    chore: add fonts

--- App.tsx
+++ App.tsx
@@ -1,17 +1,33 @@
-
 import React, { JSX } from 'react';
 
 import { StatusBar } from 'expo-status-bar';
 
 import { MovieProvider } from '@/context';
 import { Routes } from '@/routes';
-
+import useCachedResources from '@/hooks/useCachedResources';
+import { SafeAreaView } from 'react-native';
+import { Loader } from '@/components/Loader';
 
 export default function App(): JSX.Element {
+  const isLoading = useCachedResources();
+
+  if (!isLoading) {
     return (
-        <MovieProvider>
-            <Routes />
-            <StatusBar style="auto" />
-        </MovieProvider>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+        }}
+      >
+        <Loader />
+      </SafeAreaView>
     );
+  }
+
+  return (
+    <MovieProvider>
+      <Routes />
+      <StatusBar style="auto" />
+    </MovieProvider>
+  );
 }
